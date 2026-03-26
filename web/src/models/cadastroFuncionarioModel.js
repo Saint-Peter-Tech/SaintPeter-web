c9a676969d7efedb41708f83c2b48f0503467966
gustavoVbdr <gustavo.rodrigues@sptech.school>
2026-03-25 21:35:55 -0300

    Cadastro Funcionario e back adicionado (não modificado e não funcional)

---
+++ web/src/models/cadastroFuncionarioModel.js
@@ -0,0 +1,19 @@
+var database = require("../database/config");
+
+function cadastrar(nome, email, senha, cpf, idAdm, idEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf, idAdm, idEmpresa);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+            INSERT INTO usuario(nome, email, senha, cpf, ativo, fk_adm, fk_empresa) VALUES 
+            ('${nome}', '${email}', '${senha}', '${cpf}', 1, ${idAdm}, ${idEmpresa});
+                    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    cadastrar
+};
