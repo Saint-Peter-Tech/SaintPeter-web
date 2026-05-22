48b3ed7cd7919a115f87f934eb43eb5bdb367235
DiegoMenegaldo <diego.menegaldo@sptech.school>
2026-05-22 11:25:40 -0300

    Atualizando cadastro de modelos e o select do banco de dados para ficar de acordo com a atualiização da tabela

--- web/src/models/modeloModel.js
+++ web/src/models/modeloModel.js
@@ -1,25 +1,26 @@
 var database = require("../database/config");
 
-function cadastrarModelo(nomeModelo, dataLancamento, marca, descricao) {
+function cadastrarModelo(nomeModelo, dataLancamento, descricao, fkEmpresa) {
     var instrucaoSql = `
-        INSERT INTO modelos (nome, dtLancamento, marca, descricao) 
-        VALUES ('${nomeModelo}', '${dataLancamento}', '${marca}', '${descricao}');
+        INSERT INTO modelos (nome, dtLancamento, descricao, fk_empresa) 
+        VALUES ('${nomeModelo}', '${dataLancamento}', '${descricao}', ${fkEmpresa});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarModelos() {
+function buscarModelos(fkEmpresa) {
     var instrucaoSql = `
-        SELECT id_modelo, nome, DATE_FORMAT(dtLancamento, '%d/%m/%Y') AS dtLancamento, marca, descricao FROM modelos;
+        SELECT id_modelo, nome, DATE_FORMAT(dtLancamento, '%d/%m/%Y') AS dtLancamento, descricao FROM modelos
+        WHERE fk_empresa = ${fkEmpresa};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarModelosPorNome() {
+function buscarModelosPorNome(fkEmpresa) {
     var instrucaoSql = `
-        SELECT nome FROM modelos;
+        SELECT nome FROM modelos WHERE fk_empresa = ${fkEmpresa};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
