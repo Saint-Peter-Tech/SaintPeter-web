84938f48107c6628f6f6cbe7a7538cfeee56acf1
Philipi <philipi.rodrigues@sptech.school>
2026-05-27 21:06:51 -0300

    Criação de controller e model para a dash Analista

---
+++ web/src/models/analistaModel.js
@@ -0,0 +1,35 @@
+var database = require("../database/config")
+
+function autenticaremail(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT id_usuario, nome_usuario, email, cpf, fk_adm, fk_empresa FROM usuarios WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function autenticarcpf(cpf, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", cpf, senha)
+    var instrucaoSql = `
+        SELECT id_usuario, nome_usuario, email, cpf, fk_adm, fk_empresa FROM usuarios WHERE cpf = '${cpf}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function listarFuncionariosPorEmpresa(fk_empresa) {
+    var instrucaoSql = `
+        SELECT id_usuario, nome_usuario, email, cpf FROM usuarios WHERE fk_empresa = '${fk_empresa}' AND fk_adm IS NOT NULL;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    autenticaremail,
+    autenticarcpf,
+    listarFuncionariosPorEmpresa
+};
